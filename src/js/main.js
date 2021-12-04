a12ccb162d4b8d8267fe48692c4c7716f5c42f69
Silvain <silver383@gmail.com>
2021-12-04 20:57:51 +0200

    font

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
-// window.onload = function () {
-// 	fsLightboxInstances['first-lightbox'].open(0);
-// }
+window.onload = function () {
+	// fsLightboxInstances['first-lightbox'].open(0);
+	
+	
+	const phoneShow = () => {document.querySelector('.header__phone').classList.add('phone--active')}
+	const phoneHide = () => {document.querySelector('.header__phone').classList.remove('phone--active')}
+	
+	document.getElementById('phone__icon').onclick = phoneShow
+	document.getElementById('phone__close').onclick = phoneHide
+}
